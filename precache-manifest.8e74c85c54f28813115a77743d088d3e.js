3ca1abec644c3a5c9d8186404612942af1893469
WaryaWaxbaro <warya.waxbaro@gmail.com>
2019-10-13 13:53:03 +0300

    Updates

---
+++ precache-manifest.8e74c85c54f28813115a77743d088d3e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ce5457fb651215ddf838e3bdaceaf1",
+    "url": "/Baro-word-educator/index.html"
+  },
+  {
+    "revision": "0d303ccaf4cf90427bcd",
+    "url": "/Baro-word-educator/static/css/main.1641798a.chunk.css"
+  },
+  {
+    "revision": "51db9215d1a55c813522",
+    "url": "/Baro-word-educator/static/js/2.58c91f49.chunk.js"
+  },
+  {
+    "revision": "0d303ccaf4cf90427bcd",
+    "url": "/Baro-word-educator/static/js/main.69b62d97.chunk.js"
+  },
+  {
+    "revision": "3f7888654fedf9b88f04",
+    "url": "/Baro-word-educator/static/js/runtime-main.8593f076.js"
+  },
+  {
+    "revision": "4c44d6fe741be69d5bbf8f15c25fdba6",
+    "url": "/Baro-word-educator/static/media/Carrot.4c44d6fe.png"
+  },
+  {
+    "revision": "2b26f2d1def052a5f55c77b394ef6ffb",
+    "url": "/Baro-word-educator/static/media/Strawberry.2b26f2d1.png"
+  },
+  {
+    "revision": "f0ff3176d72eadf8e88d695e52e50222",
+    "url": "/Baro-word-educator/static/media/Tomatoes.f0ff3176.png"
+  },
+  {
+    "revision": "e148eb422e699c24dc5dd34903668dda",
+    "url": "/Baro-word-educator/static/media/apple.e148eb42.png"
+  },
+  {
+    "revision": "90fda5eba345d7b883f55a44b398641f",
+    "url": "/Baro-word-educator/static/media/bananas.90fda5eb.png"
+  },
+  {
+    "revision": "eb7d9ade2fcb308019cceb2eb6203aa3",
+    "url": "/Baro-word-educator/static/media/brocoli.eb7d9ade.png"
+  },
+  {
+    "revision": "bb207dc4f518382aa2d20d9131c9797d",
+    "url": "/Baro-word-educator/static/media/cucumber.bb207dc4.png"
+  },
+  {
+    "revision": "2f4425dfb0137f56a46ac34cf6ec41e2",
+    "url": "/Baro-word-educator/static/media/lime.2f4425df.png"
+  },
+  {
+    "revision": "101a49875175e6826c13c007713e8715",
+    "url": "/Baro-word-educator/static/media/orange.101a4987.png"
+  },
+  {
+    "revision": "45e198ac3adf83dc1ab43879ef7f24a5",
+    "url": "/Baro-word-educator/static/media/watermelon.45e198ac.png"
+  }
+]);
