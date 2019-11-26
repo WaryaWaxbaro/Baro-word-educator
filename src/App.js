62c28fd9acad366061f26b65cd1ffe3765fdd373
Warya Waxbaro <warya.waxbaro@gmail.com>
2019-11-26 17:42:32 +0200

    add hashrouter basename

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <HashRouter>
+      <HashRouter basename="/">
         <Navbar />
         <Container>
           <Switch>
