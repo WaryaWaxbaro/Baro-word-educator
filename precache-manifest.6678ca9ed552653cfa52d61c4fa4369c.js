707be8e3671f7a3becfb178159d50d25d30f5724
Warya Waxbaro <warya.waxbaro@gmail.com>
2019-12-03 18:20:54 +0200

    Updates

---
+++ precache-manifest.6678ca9ed552653cfa52d61c4fa4369c.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c8d00ab4d17057e043a2195346b34e",
+    "url": "/Baro-word-educator/index.html"
+  },
+  {
+    "revision": "268dfa30a3c8de86a751",
+    "url": "/Baro-word-educator/static/css/main.a2b71b69.chunk.css"
+  },
+  {
+    "revision": "a4c9b1448dd295bf1f23",
+    "url": "/Baro-word-educator/static/js/2.a80e1275.chunk.js"
+  },
+  {
+    "revision": "268dfa30a3c8de86a751",
+    "url": "/Baro-word-educator/static/js/main.0e6c3cd6.chunk.js"
+  },
+  {
+    "revision": "3f7888654fedf9b88f04",
+    "url": "/Baro-word-educator/static/js/runtime-main.8593f076.js"
+  },
+  {
+    "revision": "4127669922e8c0396442cdc38503ac05",
+    "url": "/Baro-word-educator/static/media/antelope.41276699.svg"
+  },
+  {
+    "revision": "96037b82cd18cfb7b4ce188d97d8a771",
+    "url": "/Baro-word-educator/static/media/apple.96037b82.svg"
+  },
+  {
+    "revision": "112cdfbd6eb2de4d77e2674f2bba41c2",
+    "url": "/Baro-word-educator/static/media/apples-green.112cdfbd.svg"
+  },
+  {
+    "revision": "5c9ab86514a3a95840d5e5db2688baab",
+    "url": "/Baro-word-educator/static/media/banana.5c9ab865.svg"
+  },
+  {
+    "revision": "883a77cfce21608b6f416eb71c4e6401",
+    "url": "/Baro-word-educator/static/media/broccoli.883a77cf.svg"
+  },
+  {
+    "revision": "0307abbf7c441f7aca93c5efd34c78f4",
+    "url": "/Baro-word-educator/static/media/carrot.0307abbf.svg"
+  },
+  {
+    "revision": "02eb549a579fb5d1be1c412b2d626094",
+    "url": "/Baro-word-educator/static/media/cub.02eb549a.svg"
+  },
+  {
+    "revision": "0356db94bc5afc06f72114205ade6f3b",
+    "url": "/Baro-word-educator/static/media/cucumber.0356db94.svg"
+  },
+  {
+    "revision": "8b2c1062222002994314fcc93d4e6ce6",
+    "url": "/Baro-word-educator/static/media/deer.8b2c1062.svg"
+  },
+  {
+    "revision": "d0e6c51d3fb150f290f6f30f68d51539",
+    "url": "/Baro-word-educator/static/media/dolphin.d0e6c51d.svg"
+  },
+  {
+    "revision": "8a559cee5d73bb607ff75aa961a4155a",
+    "url": "/Baro-word-educator/static/media/elephant.8a559cee.svg"
+  },
+  {
+    "revision": "69ca2609cde6c87441694709d47621a4",
+    "url": "/Baro-word-educator/static/media/finland.69ca2609.svg"
+  },
+  {
+    "revision": "b49c81bcce3e6e19e7f1b32c0ecc3076",
+    "url": "/Baro-word-educator/static/media/giraffe.b49c81bc.svg"
+  },
+  {
+    "revision": "4d75b320f3ca7a086c6bbde4799d4926",
+    "url": "/Baro-word-educator/static/media/grape.4d75b320.svg"
+  },
+  {
+    "revision": "d53072cd83024d16422903f82177c6c4",
+    "url": "/Baro-word-educator/static/media/hippo.d53072cd.svg"
+  },
+  {
+    "revision": "a890f69312b146d06947bb685407ab6f",
+    "url": "/Baro-word-educator/static/media/kangaroo.a890f693.svg"
+  },
+  {
+    "revision": "6725c4102373e33e0709fc4b8145ec46",
+    "url": "/Baro-word-educator/static/media/lime.6725c410.svg"
+  },
+  {
+    "revision": "e9e5161602f4d4839961823ff471b8bb",
+    "url": "/Baro-word-educator/static/media/lion.e9e51616.svg"
+  },
+  {
+    "revision": "769ec2734cff55522f929021d651b5f9",
+    "url": "/Baro-word-educator/static/media/logo.769ec273.svg"
+  },
+  {
+    "revision": "312189c43bd64b4726d7e18b9f7e9327",
+    "url": "/Baro-word-educator/static/media/mango.312189c4.svg"
+  },
+  {
+    "revision": "5616bd28911af3fb512386a88b215f2e",
+    "url": "/Baro-word-educator/static/media/monkey.5616bd28.svg"
+  },
+  {
+    "revision": "34fa11605783b1a592959b629e859a5c",
+    "url": "/Baro-word-educator/static/media/moose.34fa1160.svg"
+  },
+  {
+    "revision": "c1d183b381f903ec7992aa9b9d1e01ba",
+    "url": "/Baro-word-educator/static/media/orange.c1d183b3.svg"
+  },
+  {
+    "revision": "c9276cc2012607adb28a93620181325c",
+    "url": "/Baro-word-educator/static/media/pineapple.c9276cc2.svg"
+  },
+  {
+    "revision": "13a4afb42f0da727f3e39d250d9d7fb7",
+    "url": "/Baro-word-educator/static/media/polar.13a4afb4.svg"
+  },
+  {
+    "revision": "263938505cea4a29f6d7ad33de16019e",
+    "url": "/Baro-word-educator/static/media/pumpkin.26393850.svg"
+  },
+  {
+    "revision": "2838fdf4cbcbe273af1c47f3e5405444",
+    "url": "/Baro-word-educator/static/media/somalia.2838fdf4.svg"
+  },
+  {
+    "revision": "f9fef2da67ece0d8778869e38537cb4e",
+    "url": "/Baro-word-educator/static/media/squirrel.f9fef2da.svg"
+  },
+  {
+    "revision": "b7931bae80e691c7531de66c447bb572",
+    "url": "/Baro-word-educator/static/media/strawberry.b7931bae.svg"
+  },
+  {
+    "revision": "7a33c02ecfc7a0b1347ef9bfc13a9b0c",
+    "url": "/Baro-word-educator/static/media/tiger.7a33c02e.svg"
+  },
+  {
+    "revision": "386cc387066317cf209c9abf7e98afab",
+    "url": "/Baro-word-educator/static/media/united-kingdom.386cc387.svg"
+  },
+  {
+    "revision": "b9fa43314f23568e335cd6cbf066815f",
+    "url": "/Baro-word-educator/static/media/watermelon.b9fa4331.svg"
+  },
+  {
+    "revision": "4fcf7ac3517cae865dafc5554e9a5c34",
+    "url": "/Baro-word-educator/static/media/zebra.4fcf7ac3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Baro-word-educator/precache-manifest.4af82718d195d6067f4162d26cb2d41e.js"
+  "/Baro-word-educator/precache-manifest.6678ca9ed552653cfa52d61c4fa4369c.js"
 );
 
 self.addEventListener('message', (event) => {
